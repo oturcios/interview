5b7174b4004e56e283d772d7d94520f61b7cd74a
Oscar Turcios <oturcios@gk-software.com>
2021-04-15 15:08:49 -0400

    Enhancing the method in Luhn.java to handle cardNumber value in a String variable instead of int Using BigIntenger in Luhn.countRange to allow validate cardNumbers with 32+ characters

--- card-validator/src/test/java/com/hps/luhn/LuhnUnitTest.java
+++ card-validator/src/test/java/com/hps/luhn/LuhnUnitTest.java
@@ -18,25 +18,48 @@ public class LuhnUnitTest {
 
 	@Test
 	public void generateCheckDigit() {
-		assertEquals(8, luhn.generateCheckDigit(42424242));
-		assertEquals(6, luhn.generateCheckDigit(55555555));
-		assertEquals(6, luhn.generateCheckDigit(10569300));
+		assertEquals(-1, luhn.generateCheckDigit("not a number"));
+		assertEquals(-1, luhn.generateCheckDigit(null));
+		assertEquals(8, luhn.generateCheckDigit("42424242"));
+		assertEquals(6, luhn.generateCheckDigit("55555555"));
+		assertEquals(6, luhn.generateCheckDigit("10569300"));
+		assertEquals(2, luhn.generateCheckDigit("424242424242424"));
+		assertEquals(6, luhn.generateCheckDigit("400005665566555"));
+		assertEquals(1, luhn.generateCheckDigit("37144963539843"));
 	}
 
 	@Test
 	public void isValidLuhn() {
-		assertTrue(luhn.isValidLuhn(424242428));
-		assertFalse(luhn.isValidLuhn(424242421));
-		assertTrue(luhn.isValidLuhn(555555556));
-		assertFalse(luhn.isValidLuhn(555555551));
-		assertTrue(luhn.isValidLuhn(105693006));
-		assertFalse(luhn.isValidLuhn(105693001));
+		assertFalse(luhn.isValidLuhn("not a number"));
+		assertFalse(luhn.isValidLuhn(null));
+		assertTrue(luhn.isValidLuhn("424242428"));
+		assertFalse(luhn.isValidLuhn("424242421"));
+		assertTrue(luhn.isValidLuhn("555555556"));
+		assertFalse(luhn.isValidLuhn("555555551"));
+		assertTrue(luhn.isValidLuhn("105693006"));
+		assertFalse(luhn.isValidLuhn("105693001"));
+		assertFalse(luhn.isValidLuhn("4242424242424241"));
+		assertTrue(luhn.isValidLuhn("4242424242424242"));
+		assertFalse(luhn.isValidLuhn("4000056655665551"));
+		assertTrue(luhn.isValidLuhn("4000056655665556"));
+		assertFalse(luhn.isValidLuhn("371449635398432"));
+		assertTrue(luhn.isValidLuhn("371449635398431"));
 	}
 
 	@Test
 	public void countRange() {
-		assertEquals(2, luhn.countRange(927398710, 927398730));
-		assertEquals(0, luhn.countRange(927398730, 927398710));
+		assertEquals(2, luhn.countRange("927398710", "927398730"));
+		assertEquals(0, luhn.countRange("927398730", "927398710"));
+		assertEquals(6, luhn.countRange("4242424242424241", "4242424242424291"));
+		assertEquals(5, luhn.countRange("42424242424242414242424242424241", "42424242424242414242424242424291"));
+		assertEquals(0, luhn.countRange("not a number", "1"));
+		assertEquals(0, luhn.countRange("1", "not a number"));
+		assertEquals(0, luhn.countRange("not a number", "not a number either"));
+		assertEquals(0, luhn.countRange("not a number", null));
+		assertEquals(0, luhn.countRange(null, "not a number"));
+		assertEquals(0, luhn.countRange(null, "1"));
+		assertEquals(0, luhn.countRange("1", null));
+		assertEquals(0, luhn.countRange(null, null));
 	}
 
 }
